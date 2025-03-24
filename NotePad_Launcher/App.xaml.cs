4088042e35a2a156d2869fec8907ffb2215830f8
Verluer <xdbetep@gmail.com>
2025-03-24 11:28:03 +0200

    Добавьте файлы проекта.

---
+++ NotePad_Launcher/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NotePad_Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
